fe7cb65f4511e800f73738ee0a30e75260926c41
Niranjan0524 <parthalase05gmali.com@gmail.com>
2024-10-27 22:03:09 +0530

    Generate button added

--- ImprintProject/src/routes/AddCard.jsx
+++ ImprintProject/src/routes/AddCard.jsx
@@ -1,103 +1,175 @@
-import React, { useState } from 'react';
-import '../components/AddCard.css';
-import Header from '../components/Header';
-
-
+import React, { useRef, useState } from "react";
+import "../components/AddCard.css";
+import Header from "../components/Header";
 
 const AddCard = () => {
-  const [form, setForm] = useState({
-    type: '',
-    deck: '',
-    front: '',
-    back: '',
-    tags: '',
-  });
+  const [notification, setNotification] = useState("");
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setForm({
-      ...form,
-      [name]: value,
-    });
-  };
+  const deckNameRef = useRef("");
+  const frontRef = useRef("");
+  const backRef = useRef("");
+  const tagsRef = useRef("");
+
+  const [mcq, setMcq] = useState(false);
 
   const handleSubmit = () => {
-    console.log(form);
-    // Add form submission logic
+    const deckNamevalue = deckNameRef.current.value;
+    const frontvalue = frontRef.current.value;
+    const backvalue = backRef.current.value;
+    const tagsValue = tagsRef.current.value;
+
+    console.log(deckNamevalue);
+    deckNameRef.current.value = "";
+    frontRef.current.value = "";
+    backRef.current.value = "";
+    tagsRef.current.value = "";
+
+    setNotification(`Card added successfully!`);
+    setTimeout(() => {
+      setNotification('');
+    }, 3000);
   };
 
+  const handleMcq = () => {
+    console.log("MCQ");
+    setMcq(true);
+  };
+
+  const handleBasic = () => {
+    console.log("Basic");
+    setMcq(false);
+  };
+
+  const handleGenerate = () => {
+    console.log("Generate");
+  };
   return (
     <>
-    <div className="AddCardContainer">
-    <Header/>
+      <div className="AddCardContainer">
+        <Header />
+        {notification && <div className="notification">{notification}</div>}
 
+        <div className="container2">
+          <center>
+            <div className="Heading">Make A New Card</div>
+          </center>
+          <div className="AddcardFrom">
+            <div class="row mb-3">
+              <label for="inputPassword3" class="col-sm-2 col-form-label">
+                Deck
+              </label>
+              <div class="col-sm-10">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="DeckName"
+                  placeholder="DeckName"
+                  ref={deckNameRef}
+                />
+              </div>
+            </div>
 
-    <div className="container2">
-      <center>
-        <div className="Heading">
-          Make A New Card
+            <div class="row mb-3">
+              <label for="inputPassword3" class="col-sm-2 col-form-label">
+                Front
+              </label>
+              <div class="col-sm-10">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="Front"
+                  placeholder="Front"
+                  ref={frontRef}
+                />
+              </div>
+            </div>
+
+            <div class="row mb-3">
+              <label for="inputPassword3" class="col-sm-2 col-form-label">
+                Back
+              </label>
+              <div class="col-sm-10">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="Back"
+                  placeholder="Back"
+                  ref={backRef}
+                />
+              </div>
+            </div>
+
+            <div class="row mb-3">
+              <label for="inputPassword3" class="col-sm-2 col-form-label">
+                Tags
+              </label>
+              <div class="col-sm-10">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="Tags"
+                  placeholder="One or Multiple"
+                  ref={tagsRef}
+                />
+              </div>
+            </div>
+
+            <fieldset class="row mb-3">
+              <legend class="col-form-label col-sm-2 pt-0">Type:</legend>
+              <div class="col-sm-10">
+               
+
+                <input
+                  type="radio"
+                  className="btn-check"
+                  name="options-outlined"
+                  id="success-outlined"
+                  autocomplete="off"
+                  checked
+                />
+                <label
+                  className="b btn btn-outline-info"
+                  for="success-outlined"
+                  onClick={handleBasic}
+                >
+                  Basic
+                </label>
+
+                <input
+                  type="radio"
+                  className="btn-check"
+                  name="options-outlined"
+                  id="danger-outlined"
+                  autocomplete="off"
+                />
+                <label
+                  className="b btn btn-outline-danger"
+                  for="danger-outlined"
+                  onClick={handleMcq}
+                >
+                  MCQ
+                </label>
+              </div>
+            </fieldset>
+
+            {mcq && (
+              <div className="generateWithAI">
+                <button className="generate-btn" onClick={handleGenerate}>
+                  Generate with AI
+                </button>
+              </div>
+            )}
+
+            <button
+              type="submit"
+              class="AddCardButton btn btn-primary"
+              onClick={handleSubmit}
+            >
+              Add Card
+            </button>
+          </div>
         </div>
-      </center>
-    <form className='AddcardFrom'>
-    
- 
-
-
-  <div class="row mb-3">
-    <label for="inputPassword3" class="col-sm-2 col-form-label">Deck</label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control" id="DeckName" placeholder='DeckName' />
-    </div>
-  </div>
-
-  
-  <div class="row mb-3">
-    <label for="inputPassword3" class="col-sm-2 col-form-label">Front</label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control" id="Front" placeholder='Front'/>
-    </div>
-  </div>
-
-
-    
-  <div class="row mb-3">
-    <label for="inputPassword3" class="col-sm-2 col-form-label">Back</label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control" id="Back" placeholder='Back'/>
-    </div>
-  </div>
-
-  <div class="row mb-3">
-    <label for="inputPassword3" class="col-sm-2 col-form-label">Tags</label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control" id="Tags" placeholder='One or Multiple' />
-    </div>
-  </div>
-
-  <fieldset class="row mb-3">
-    <legend class="col-form-label col-sm-2 pt-0">Type:</legend>
-    <div class="col-sm-10">
-      <div class="form-check">
-        <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked />
-        <label class="form-check-label" for="gridRadios1">
-          Basic
-        </label>
       </div>
-      <div class="form-check">
-        <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2" />
-        <label class="form-check-label" for="gridRadios2">
-          Optional
-        </label>
-      </div>
-      
-    </div>
-  </fieldset>
-
-  
-  <button type="submit" class="AddCardButton btn btn-primary">Add Card</button>
-</form>
-    </div>
-    </div>
     </>
   );
 };
